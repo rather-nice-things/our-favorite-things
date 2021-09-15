059c9bfb9c171bebda4411968bfa862c69e7f7a8
carinij <carinij@gmail.com>
2021-09-15 16:51:01 -0700

    Added POST route to /editor for adding new blog posts

--- server/server.js
+++ server/server.js
@@ -1,18 +1,32 @@
 const express = require('express')
 const expressStaticGzip = require('express-static-gzip')
 const app = express()
+const db = require('../database/database.js')
 
-const host = "localhost";
-const port = "3000";
+const host = 'localhost'
+const port = '3000'
 
 app.use(
-  expressStaticGzip("public", {
+  expressStaticGzip('public', {
     enableBrotli: true,
-    orderPreference: ["br", "gz"],
+    orderPreference: ['br', 'gz'],
     setHeaders: function (res, path) {
-      res.setHeader("Cache-Control", "public, max-age=31536000");
+      res.setHeader('Cache-Control', 'public, max-age=31536000')
     },
   })
 )
 
-module.exports = app;
+app.use(express.json())
+
+app.post('/editor', function (req, res) {
+  console.log('POST request to /editor')
+  db.addBlogEntry(req.body, (err, entry) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(entry);
+    }
+  })
+})
+
+module.exports = app
