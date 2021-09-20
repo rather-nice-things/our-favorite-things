f57d31248a62c5736b02ce1c59b160d9da71483b
carinij <carinij@gmail.com>
2021-09-20 08:10:46 -0700

    Added layout to _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
+import Layout from '../components/layout'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <div className="main-container">
+        <Component {...pageProps} />
+      </div>
+    </Layout>
+  )
 }
 
 export default MyApp
