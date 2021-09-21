62698e52dabd0182f9a2ca6a132e2a5e0b6afc6b
carinij <carinij@gmail.com>
2021-09-20 20:47:05 -0700

    Fixed issue with map()

--- components/blogEntryGroup.jsx
+++ components/blogEntryGroup.jsx
@@ -2,13 +2,11 @@ import BlogEntry from './blogEntry.jsx'
 
 export default function BlogEntryGroup(props) {
   const blogEntryList = props.blogEntryList;
-  console.log(blogEntryList);
   return (
     <div>
       {
         blogEntryList.map((entry, index) => {
-          console.log("entry from BlogEntryGroup: ", entry);
-          <BlogEntry entry={entry} />
+          return <BlogEntry entry={entry} key={entry.title}/>
         })
       }
     </div>
